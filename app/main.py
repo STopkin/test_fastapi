925c0a3d34ae456f816fb180fbcf90ca8ab3aa02
STopkin <stopkin0@gmail.com>
2022-05-23 15:31:46 +0700

    documented

--- app/main.py
+++ app/main.py
@@ -11,7 +11,39 @@ import json
 models.Base.metadata.create_all(bind=engine)
 
 
-app = FastAPI(swagger_ui_parameters={"deepLinking": False, "syntaxHighlight.theme": "obsidian"})
+description = """
+test_fastapi API это моё тестовое задание по fastapi
+
+## Authors - авторы
+
+Можно:
+* создать автора, 
+* получить информацию по автору, включая список его публикаций
+* получить список всех авторов и количество его публикаций
+
+## Posts - публикации
+
+Можно:
+* создать публикцию
+"""
+
+
+app = FastAPI(
+   title="test_fastapi",
+    description=description,
+    version="0.0.1",
+#    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Toporkov Sergey",
+        "url": "https://github.com/STopkin",
+        "email": "stopkin0@gmail.com",
+    },
+#    license_info={
+#        "name": "Apache 2.0",
+#        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+#    },
+    swagger_ui_parameters={"deepLinking": False, "syntaxHighlight.theme": "obsidian"}
+)
 
 
 # Dependency
@@ -26,6 +58,9 @@ def get_db():
 # получить автора и список его публикаций по ID
 @app.get("/author/{author_id}", response_model=schemas.Author)
 async def get_author(author_id: int, db: Session = Depends(get_db)):
+    '''
+    Получить информацию по автору и список его публикаций по author_id
+    '''
     db_author = crud.get_author(db, author_id)
     if not db_author:
         raise HTTPException(status_code=404, detail="Author not found")
