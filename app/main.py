5c9cb4e61fd883879adc3f48179f44f17a694413
STopkin <stopkin0@gmail.com>
2022-05-23 15:34:40 +0700

    documented 2

--- app/main.py
+++ app/main.py
@@ -70,6 +70,7 @@ async def get_author(author_id: int, db: Session = Depends(get_db)):
 # создать автора
 @app.post("/author", response_model=schemas.Author)
 async def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    ''' создать автора '''
     db_author = crud.get_author_by_email(db, email=author.email)
     if db_author:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -79,6 +80,7 @@ async def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_
 # создать публикацию у автора    
 @app.post("/author/{author_id}")
 async def create_post(author_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+    ''' создать публикацию у автора '''
     db_author = crud.get_author(db, author_id)
     if not db_author:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -88,6 +90,7 @@ async def create_post(author_id: int, post: schemas.PostCreate, db: Session = De
 # получить всех авторов и кол-во публикаций
 @app.get("/authors")
 async def get_authors_and_posts(db: Session = Depends(get_db)):
+    ''' получить всех авторов и кол-во публикаций '''
     return crud.get_author_and_post_count(db)
     
     
